3f5eb897684a014ad342fd3f9894f1f29dc9bba1
Danil Pismenny <danil@brandymint.ru>
2016-12-02 21:08:17 +0300

    minor changes in ./start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 docker run -it -e SERVER_URL=teamcity.brandymint.ru \
+  --name=agent1
+  --restart=always
   -v /opt/teamcity_agent_conf:/data/teamcity_agent/conf \
   -v /var/run/postgresql/:/var/run/postgresql/ \
-  -e AGENT_NAME=srv2-agent1-v1 \
+  -e AGENT_NAME=agent1 \
   dapi/teamcity-agent
