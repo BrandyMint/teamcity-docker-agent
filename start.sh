b332119199c091568313f1a31c7d6a2a26218aa7
Danil Pismenny <dapi@users.noreply.github.com>
2017-03-20 11:54:31 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ docker run -it -e SERVER_URL=teamcity.brandymint.ru \
   -v /opt/teamcity_agent_conf:/data/teamcity_agent/conf \
   -v /var/run/postgresql/:/var/run/postgresql/ \
   -e AGENT_NAME=agent1 \
-  dapi/teamcity-agent
+  brandhmint/teamcity-agent
